d1d9dab34b99b705250d648efea953451195c46d
Michael <hitzker.michael@gmail.com>
2019-06-28 23:15:06 +0200

    Worked on manual flight gui

---
+++ src/components/game-pad/dual/nipplehtml.ts
@@ -0,0 +1,22 @@
+export const nipplejshtml = `<html style='overflow: hidden'>
+  <body>
+    <div
+      id="zone_joystick"
+      style="
+  position: absolute;
+  height: 50%;
+  width: 100%;
+  top: 0; overflow: hidden;"
+    ></div>
+    <div
+      id="zone_joystick2"
+      style="
+  position: absolute;
+  height: 50%;
+  width: 100%;
+  top: 50%;"
+    ></div>
+  </body>
+  <script src='./web/index.html'></script>
+  <script></script>
+</html>`;
--- src/screens/Fly.tsx
+++ src/screens/Fly.tsx
@@ -1,80 +1,176 @@
 import React, { Component } from 'react';
-import { View, Dimensions, Text } from 'react-native';
-import MapView, {
-  MAP_TYPES,
-  PROVIDER_DEFAULT,
-  UrlTile
-} from 'react-native-maps';
+import {
+  View,
+  Dimensions,
+  Text,
+  WebView,
+  SafeAreaView,
+  TouchableOpacity
+} from 'react-native';
 
-import MapComponent from '../components/Map';
-import RNGamePad from 'react-native-game-pad';
+import RNGamePadDual from '../components/game-pad/dual/dual-joystick';
+import colors from '../colors';
+import Icon from 'react-native-vector-icons/Ionicons';
 import { Fonts } from '../fonts';
+import ManualFlightButton from '../components/ManualFlightButton';
 
-interface Props {
-  provider: any;
-}
+interface Props {}
 interface State {
-  region: {
-    latitude: number;
-    longitude: number;
-    latitudeDelta: number;
-    longitudeDelta: number;
-  };
-  isMapReady?: boolean;
+  altHoldText: string;
+  altHoldIcon: string;
+  armText: string;
+  armIcon: string;
 }
 const { width, height } = Dimensions.get('window');
 
-const ASPECT_RATIO = width / height;
-const LATITUDE = 22.720555;
-const LONGITUDE = 75.858633;
-const LATITUDE_DELTA = 0.0922;
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-
 const options = {
-  color: '#ff0000',
-  size: 10
+  color: colors.primaryColor,
+  size: 500
 };
 
 class Fly extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      region: {
-        latitude: LATITUDE,
-        longitude: LONGITUDE,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA
-      }
+      altHoldText: 'ALT HOLD ON',
+      altHoldIcon: 'md-arrow-round-up',
+      armText: 'ARM',
+      armIcon: 'md-sync'
     };
   }
 
-  onMapLayout = () => {
-    this.setState({ isMapReady: true });
-  };
-
-  get mapType() {
-    return this.props.provider === PROVIDER_DEFAULT
-      ? MAP_TYPES.STANDARD
-      : MAP_TYPES.NONE;
-  }
   render() {
     return (
-      <View
-        style={{
-          height: '100%',
-          width: '100%',
-          justifyContent: 'center',
-          alignContent: 'center',
-          padding: 20
-        }}
-      >
-        <Text style={{ fontFamily: Fonts.Roboto.bold, color: '#000000' }}>
-          This does not work atm because the repo
-          https://github.com/jim-at-jibba/react-native-game-pad does not work!
-        </Text>
-      </View>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View
+          style={{
+            height: '100%',
+            width: '100%',
+            justifyContent: 'center',
+            alignContent: 'center',
+            padding: 20
+          }}
+        >
+          <RNGamePadDual
+            style={{ width: '50%', height: '50%' }}
+            options={options}
+            options2={options}
+            onRightMove={this.handleOnRightMove}
+            onLeftMove={this.handleOnLeftMove}
+          />
+          <View
+            style={{
+              flexDirection: 'column',
+              position: 'absolute',
+              top: 80,
+              right: -50,
+              justifyContent: 'flex-start',
+              alignItems: 'center'
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: Fonts.Roboto.bold,
+                width: 160,
+                transform: [{ rotate: '90deg' }],
+                color: 'black'
+              }}
+            >
+              Altitude Hold is{' '}
+              {this.state.altHoldIcon === 'md-pause' ? 'ON' : 'OFF'}
+            </Text>
+            <View
+              style={{
+                width: 15,
+                height: 15,
+                backgroundColor:
+                  this.state.altHoldIcon === 'md-pause' ? '#27ae60' : '#c0392b',
+                borderRadius: 15,
+                marginTop: 45
+              }}
+            />
+          </View>
+
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100%',
+              position: 'absolute',
+              marginLeft: 20
+            }}
+          >
+            <ManualFlightButton
+              style={{
+                transform: [{ rotate: '90deg' }],
+                margin: 10,
+                width: 70
+              }}
+              text='HOME'
+              onPress={() => {}}
+              iconName='md-home'
+            />
+            <ManualFlightButton
+              style={{
+                transform: [{ rotate: '90deg' }],
+                margin: 10,
+                width: 70
+              }}
+              text={this.state.altHoldText}
+              onPress={() => this.hanldeAltHoldPressed()}
+              iconName={this.state.altHoldIcon}
+            />
+            <ManualFlightButton
+              style={{
+                transform: [{ rotate: '90deg' }],
+                margin: 10,
+                width: 70
+              }}
+              text={this.state.armText}
+              onPress={() => this.handleArmPressed()}
+              iconName={this.state.armIcon}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
     );
   }
+
+  handleOnRightMove = (evt, data) => {
+    console.log('handleOnRightMove', evt, data);
+  };
+
+  handleOnLeftMove = (evt, data) => {
+    console.log('handleOnLeftMove', evt, data);
+  };
+
+  handleArmPressed = () => {
+    if (this.state.armIcon === 'md-sync') {
+      this.setState({
+        armText: 'STOP',
+        armIcon: 'md-hand'
+      });
+    } else {
+      this.setState({
+        armText: 'ARM',
+        armIcon: 'md-sync'
+      });
+    }
+  };
+
+  hanldeAltHoldPressed = () => {
+    if (this.state.altHoldIcon === 'md-arrow-round-up') {
+      this.setState({
+        altHoldText: 'ALT HOLD OFF',
+        altHoldIcon: 'md-pause'
+      });
+    } else {
+      this.setState({
+        altHoldText: 'ALT HOLD ON',
+        altHoldIcon: 'md-arrow-round-up'
+      });
+    }
+  };
 }
 export default Fly;
 
